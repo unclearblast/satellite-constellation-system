340036f94acf203a153db2a355dde2b69f36b093
unclearblast <derenikhm@gmail.com>
2026-02-20 19:16:06 +0300

    Create SatelliteState.java

---
+++ src/main/java/com/example/space/domain/satellite/SatelliteState.java
@@ -0,0 +1,29 @@
+package com.example.space.domain.satellite;
+
+public class SatelliteState {
+
+    private boolean active;
+    private String statusMessage = "Не активирован";
+
+    public void activate() {
+        active = true;
+        statusMessage = "Активен";
+    }
+
+    public void deactivate(String reason) {
+        active = false;
+        statusMessage = reason;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    @Override
+    public String toString() {
+        return "SatelliteState{" +
+                "isActive=" + active +
+                ", statusMessage='" + statusMessage + '\'' +
+                '}';
+    }
+}
