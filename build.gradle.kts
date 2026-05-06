0fec7339b2200630558e687520029118c00b8417
unclearblast <derenikhm@gmail.com>
2026-05-06 14:16:23 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,6 +26,10 @@ subprojects {
     dependencies {
         implementation("org.springframework.boot:spring-boot-starter")
         testImplementation("org.springframework.boot:spring-boot-starter-test")
+        implementation("net.devh:grpc-client-spring-boot-starter:3.0.0.RELEASE")
+        implementation("io.grpc:grpc-protobuf:1.62.2")
+        implementation("io.grpc:grpc-stub:1.62.2")
+        compileOnly("org.apache.tomcat:annotations-api:6.0.53")
     }
 
     tasks.withType<Test> {
